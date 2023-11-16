c9822c9492fab16d7745026d36bda43c30067808
Marcin Domurad <marcin.domurad@hagergroup.com>
2023-11-16 10:29:09 +0100

    new file for plugins for navigation

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -205,14 +205,4 @@ return {
       require("telescope").load_extension("file_browser")
     end,
   },
-  -- NAVIGATION
-  {
-    "phaazon/hop.nvim",
-    event = "BufRead",
-    config = function()
-      require("hop").setup()
-      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
-    end,
-  },
 }
