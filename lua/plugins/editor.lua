8b1eb11f4951583850eb01883e2ca7210858d82d
md <marcindomurad@gmail.com>
2023-11-18 21:57:50 +0100

    color highlighting plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -255,11 +255,26 @@ return {
       require("telescope").load_extension("file_browser")
     end,
   },
-  -- Color Picker
+  -- color picker
   {
     "ziontee113/color-picker.nvim",
     config = function()
       require("color-picker")
+    end,
+  },
+  -- color highlighting
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+        RGB = true, -- #RGB hex codes
+        RRGGBB = true, -- #RRGGBB hex codes
+        RRGGBBAA = true, -- #RRGGBBAA hex codes
+        rgb_fn = true, -- CSS rgb() and rgba() functions
+        hsl_fn = true, -- CSS hsl() and hsla() functions
+        css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+        css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      })
     end,
   },
 }
