678e2e7814924a8a0be9f3454e3b1610fb095825
Marcin Domurad <marcin.domurad@hagergroup.com>
2024-04-22 11:15:41 +0200

    refactor(prompts): simplify prompt messages

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -30,11 +30,7 @@ return {
       },
       prompts = {
         QuickCommit = {
-          prompt = [[ Write commit title for the change with commitizen convention.
-            Use provided git diff data for generating commit.
-            Make sure the title has maximum 50 characters.
-            Do not provide any extra information, just the title itself in one line.
-            Do not wrap the title with any symbols like qute.]],
+          prompt = "Write commit title for the change with commitizen convention. Use provided git diff data for generating commit. Make sure the title has maximum 50 characters. Do not provide any extra information, just the title itself in one line. Do not wrap the title with any symbols like qute.",
           description = "Stage all and quick commit",
           mapping = ";q",
           close = true,
@@ -54,11 +50,7 @@ return {
           end,
         },
         QuickCommitStaged = {
-          prompt = [[ Write commit title for the change with commitizen convention.
-            Use provided git diff data for generating commit.
-            Make sure the title has maximum 50 characters.
-            Do not provide any extra information, just the title itself in one line.
-            Do not wrap the title with any symbols like qute.]],
+          prompt = "Write commit title for the change with commitizen convention. Use provided git diff data for generating commit. Make sure the title has maximum 50 characters. Do not provide any extra information, just the title itself in one line. Do not wrap the title with any symbols like qute.",
           description = "Commit staged with message",
           mapping = ";c",
           close = true,
