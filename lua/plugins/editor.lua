3f9765359df21a4b6a2bcd5104972719bff623b4
md <marcindomurad@gmail.com>
2024-04-23 21:59:23 +0200

    refactor(editor): remove git.nvim plugin configuration

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -34,20 +34,6 @@ return {
       },
     },
   },
-
-  {
-    "dinhhuy258/git.nvim",
-    event = "BufReadPre",
-    opts = {
-      keymaps = {
-        -- Open blame window
-        blame = "<Leader>gb",
-        -- Open file/folder in git repository
-        browse = "<Leader>go",
-      },
-    },
-  },
-
   {
     "telescope.nvim",
     dependencies = {
