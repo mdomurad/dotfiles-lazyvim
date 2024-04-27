6bb2566d638f07c6805638adf76d0b8934826ed3
md <marcindomurad@gmail.com>
2024-04-27 12:55:26 +0200

    feat: enhance keymaps for oil.nvim plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -2,7 +2,31 @@ return {
   -- File Explorer
   {
     "stevearc/oil.nvim",
-    opts = {},
+    opts = {
+      delete_to_trash = true,
+      keymaps = {
+        ["h"] = "actions.parent",
+        ["g"] = "actions.toggle_hidden",
+        ["g\\"] = "actions.toggle_trash",
+        ["q"] = "actions.close",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-s>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-r>"] = "actions.refresh",
+        ["ocd"] = "actions.open_cwd",
+        ["cd"] = "actions.cd",
+        ["cwd"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
+        ["yp"] = "actions.copy_entry_path",
+        ["tp"] = "actions.open_cmdline",
+        ["td"] = "actions.open_cmdline_dir",
+        ["m"] = "actions.add_to_loclist",
+      },
+    },
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
