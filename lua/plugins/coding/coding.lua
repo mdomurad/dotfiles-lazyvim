5d74434fda795e75217172e67b32de9b73739ca8
md <marcindomurad@gmail.com>
2024-12-31 17:14:30 +0100

    feat(coding.lua): add emoji.nvim plugin integration

--- lua/plugins/coding/coding.lua
+++ lua/plugins/coding/coding.lua
@@ -71,10 +71,25 @@ return {
     end,
   },
   {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
+    "allaman/emoji.nvim",
+    ft = "markdown", -- adjust to your needs
+    dependencies = {
+      -- util for handling paths
+      "nvim-lua/plenary.nvim",
+      -- optional for telescope integration
+      "nvim-telescope/telescope.nvim",
+      -- optional for fzf-lua integration via vim.ui.select
+      "ibhagwan/fzf-lua",
+    },
+    opts = {
+      -- default is false, also needed for blink.cmp integration!
+      enable_cmp_integration = true,
+    },
+    config = function(_, opts)
+      require("emoji").setup(opts)
+      -- optional for telescope integration
+      local ts = require("telescope").load_extension("emoji")
+      vim.keymap.set("n", "<leader>se", ts.emoji, { desc = "[S]earch [E]moji" })
     end,
   },
 }
