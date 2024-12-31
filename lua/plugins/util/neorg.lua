a2a1e2bc9188421a51fc03e4c20d15469ad59e52
md <marcindomurad@gmail.com>
2024-12-31 17:10:00 +0100

    chore(neorg.lua): comment out core.completion config

--- lua/plugins/util/neorg.lua
+++ lua/plugins/util/neorg.lua
@@ -17,7 +17,7 @@ return {
       require("neorg").setup({
         load = {
           ["core.defaults"] = {}, -- Loads default behaviour
-          ["core.completion"] = { config = { engine = "nvim-cmp" } },
+          -- ["core.completion"] = { config = { engine = "nvim-cmp" } },
           ["core.concealer"] = {}, -- Adds pretty icons to your documents
           ["core.dirman"] = { -- Manages Neorg workspaces
             config = {
