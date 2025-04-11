b441206f572c4e3e44dbed9a7799f9f7e7468554
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-04-11 16:37:17 +0200

    chore: disable roslyn plugin and easy-dotnet till fixed

--- lua/plugins/lang/net.lua
+++ lua/plugins/lang/net.lua
@@ -1,9 +1,18 @@
 return {
-  {
-    "GustavEikaas/easy-dotnet.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-    config = function()
-      require("easy-dotnet").setup({})
-    end,
-  },
+  -- "seblyng/roslyn.nvim",
+  -- ft = "cs",
+  -- ---@module 'roslyn.config'
+  -- ---@type RoslynNvimConfig
+  -- opts = {
+  -- },
+  -- Configuration is throwing an error
+  -- TODO: track issue
+  -- https://github.com/GustavEikaas/easy-dotnet.nvim/issues/304
+  -- {
+  --   "GustavEikaas/easy-dotnet.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  --   config = function()
+  --     require("easy-dotnet").setup()
+  --   end,
+  -- },
 }
