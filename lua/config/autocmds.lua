8122dec8a2ad36ffffeeb0466f8176f48206b20d
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-04-15 13:21:19 +0200

    chore(autocmds roslyn): refresh diagnostic on InsertLeave

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -55,3 +55,19 @@ vim.api.nvim_create_autocmd("Filetype", {
     vim.keymap.set("n", "gx", "<Plug>(neorg.esupports.hop.hop-link)", { buffer = true })
   end,
 })
+
+-- Roslyn
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  pattern = "*",
+  callback = function()
+    local clients = vim.lsp.get_clients({ name = "roslyn" })
+    if not clients or #clients == 0 then
+      return
+    end
+
+    local buffers = vim.lsp.get_buffers_by_client_id(clients[1].id)
+    for _, buf in ipairs(buffers) do
+      vim.lsp.util._refresh("textDocument/diagnostic", { bufnr = buf })
+    end
+  end,
+})
