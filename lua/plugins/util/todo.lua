e0b31b48f99515d19f8d56fcb3cb6560c718ede2
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-07-02 12:52:01 +0200

    feat(util/todo.lua): configure dooing window size

--- lua/plugins/util/todo.lua
+++ lua/plugins/util/todo.lua
@@ -1,6 +1,11 @@
 return {
   "atiladefreitas/dooing",
   config = function()
-    require("dooing").setup({})
+    require("dooing").setup({
+      window = {
+        width = 90,
+        height = 35,
+      },
+    })
   end,
 }
