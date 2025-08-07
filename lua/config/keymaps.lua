8dd1f5002cc31cdc4e0eb502da9a14328d702e7f
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-08-07 14:28:32 +0200

    refactor(keymaps.lua): remove split window keymaps for cleaner config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -39,10 +39,6 @@ keymap.set("n", "<C-m>", "<C-i>", opts)
 -- New tab
 keymap.set("n", "te", ":tabedit")
 
--- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
-
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
