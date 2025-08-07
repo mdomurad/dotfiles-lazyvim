9fc8516652834f3da512adc3d2a36bcc0367bb59
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-08-07 14:40:29 +0200

    feat(navigation): change hop word kemap

--- lua/plugins/ui/navigation.lua
+++ lua/plugins/ui/navigation.lua
@@ -4,8 +4,7 @@ return {
     event = "BufRead",
     config = function()
       require("hop").setup()
-      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
-      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "F", ":HopWord<CR>", { noremap = true, silent = true })
     end,
   },
 }
