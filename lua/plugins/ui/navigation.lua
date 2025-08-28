bc05c71b94aca239082994f089d982d0c11f2df2
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-08-28 12:05:46 +0200

    fix(FilteringService.cs): correct selection logic in FilterSelect method

--- lua/plugins/ui/navigation.lua
+++ lua/plugins/ui/navigation.lua
@@ -1,10 +1,28 @@
 return {
   {
-    "phaazon/hop.nvim",
-    event = "BufRead",
+    "smoka7/hop.nvim",
+    version = "*",
     config = function()
       require("hop").setup()
-      vim.api.nvim_set_keymap("n", "F", ":HopWord<CR>", { noremap = true, silent = true })
+      -- vim.api.nvim_set_keymap("n", "F", ":HopWord<CR>", { noremap = true, silent = true })
+
+      local hop = require("hop")
+      local directions = require("hop.hint").HintDirection
+      vim.keymap.set("", "f", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+      end, { remap = true })
+      vim.keymap.set("", "F", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+      end, { remap = true })
+      vim.keymap.set("", "t", function()
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })
+      end, { remap = true })
+      vim.keymap.set("", "T", function()
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })
+      end, { remap = true })
+      vim.keymap.set("", "T", function()
+        hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
+      end, { remap = true })
     end,
   },
 }
