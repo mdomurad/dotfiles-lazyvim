b8d7cf5b1eded3a9e7ba15eb1307bf650c779c53
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-08-29 12:21:37 +0200

    feat(ai.lua): add mcphub extension support to codeCompanion configuration

--- lua/plugins/ai/ai.lua
+++ lua/plugins/ai/ai.lua
@@ -267,12 +267,23 @@ local codeCompanion = {
         inline = {
           adapter = "copilot",
         },
+        extensions = {
+          mcphub = {
+            callback = "mcphub.extensions.codecompanion",
+            opts = {
+              make_vars = true,
+              make_slash_commands = true,
+              show_result_in_chat = true,
+            },
+          },
+        },
       },
     })
   end,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+    "ravitemer/mcphub.nvim",
   },
 }
 
