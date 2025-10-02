3a5ffe2ce08b58415bc1e069d2fa8efe174f5f58
Marcin Domurad <marcin.domurad@hagergroup.com>
2025-10-02 09:42:52 +0200

    feat(ui): move lualine config to separate file and add sidekick status indicator

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -3,5 +3,6 @@ return {
   require("plugins.ui.filebrowser"),
   require("plugins.ui.editor"),
   require("plugins.ui.navigation"),
+  require("plugins.ui.lualine"),
   require("plugins.ui.ui"),
 }
---
+++ lua/plugins/ui/lualine.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_c, {
+        function()
+          return " "
+        end,
+        color = function()
+          local status = require("sidekick.status").get()
+          if status then
+            return status.kind == "Error" and "DiagnosticError" or status.busy and "DiagnosticWarn" or "Special"
+          end
+        end,
+        cond = function()
+          local status = require("sidekick.status")
+          return status.get() ~= nil
+        end,
+      })
+    end,
+  },
+}
