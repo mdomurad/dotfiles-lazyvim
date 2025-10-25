9de5cba7ddcd097369c64840a5bca9b6d60c5fb7
md <marcindomurad@gmail.com>
2025-10-25 15:16:02 +0200

    feat(net.lua): switch easy-dotnet picker to fzf-lua

--- lua/plugins/lang/net.lua
+++ lua/plugins/lang/net.lua
@@ -49,9 +49,10 @@ return {
   },
   {
     "GustavEikaas/easy-dotnet.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "ibhagwan/fzf-lua" },
     config = function()
       require("easy-dotnet").setup({
+        picker = "fzf",
         test_runner = {
           viewmode = "float",
           mappings = {
