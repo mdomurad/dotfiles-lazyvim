1ba2f536f5e9b298aeb29ef537c9970f0124385c
md <marcindomurad@gmail.com>
2025-10-25 15:54:17 +0200

    refactor(editor.lua): replace Telescope with fzf-lua for file and symbol pickers

--- lua/plugins/ui/editor.lua
+++ lua/plugins/ui/editor.lua
@@ -24,25 +24,15 @@ return {
   -- Multiple cursors plugin
   { "mg979/vim-visual-multi" },
 
-  -- Telescop File Browser
   {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  },
-  {
-    "telescope.nvim",
-    dependencies = {
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-      },
-      "nvim-telescope/telescope-file-browser.nvim",
-    },
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
       {
         "<leader>fP",
         function()
-          require("telescope.builtin").find_files({
+          require("fzf-lua").files({
             cwd = require("lazy.core.config").options.root,
           })
         end,
@@ -51,10 +41,9 @@ return {
       {
         ";f",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.find_files({
-            no_ignore = false,
-            hidden = true,
+          require("fzf-lua").files({
+            git_icons = false,
+            file_icons = false,
           })
         end,
         desc = "Lists files in your current working directory, respects .gitignore",
@@ -62,212 +51,65 @@ return {
       {
         ";r",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.live_grep()
+          require("fzf-lua").live_grep()
         end,
         desc = "Search for a string in your current working directory and get results live as you type, respects .gitignore",
       },
       {
-        "\\\\",
+        "\\",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.buffers()
+          require("fzf-lua").buffers()
         end,
         desc = "Lists open buffers",
       },
       {
         ";t",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.help_tags()
+          require("fzf-lua").help_tags()
         end,
         desc = "Lists available help tags and opens a new window with the relevant help info on <cr>",
       },
       {
         ";.",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.resume()
+          require("fzf-lua").resume()
         end,
-        desc = "Resume the previous telescope picker",
+        desc = "Resume the previous fzf-lua picker",
       },
       {
         ";d",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.diagnostics()
+          require("fzf-lua").diagnostics_document()
         end,
-        desc = "Lists Diagnostics for all open buffers or a specific buffer",
+        desc = "Lists Diagnostics for current buffer",
       },
       {
         ";w",
         function()
-          local builtin = require("telescope.builtin")
-          local clients = vim.lsp.get_clients()
-          -- Check if there are any language server clients
-          if #clients > 0 then
-            local csharp_client = nil
-            for _, client in ipairs(clients) do
-              if client.name == "csharp_ls" then
-                csharp_client = client
-                break
-              end
-            end
-            -- If a C# language server is found, request code actions for the workspace
-            if csharp_client then
-              csharp_client.request("textDocument/definition", {
-                textDocument = { uri = vim.uri_from_bufnr(0) },
-                range = { start = { line = 0, character = 0 }, ["end"] = { line = vim.fn.line("$"), character = 0 } },
-                context = { diagnostics = {} },
-              }, function(_, _, actions)
-                -- Filter and display only the diagnostics from the code actions
-                local diagnostics = {}
-                for _, action in ipairs(actions) do
-                  if action.kind and action.kind.value == "quickfix" and action.diagnostics then
-                    for _, diagnostic in ipairs(action.diagnostics) do
-                      table.insert(diagnostics, diagnostic)
-                    end
-                  end
-                end
-                if #diagnostics > 0 then
-                  builtin.quickfix({ entries = diagnostics })
-                else
-                  print("No diagnostics found for the workspace.")
-                end
-              end)
-            else
-              -- If no C# language server is found, fall back to listing diagnostics for all open buffers
-              builtin.diagnostics()
-            end
-          else
-            -- If no language server clients are available, fall back to listing diagnostics for all open buffers
-            builtin.diagnostics()
-          end
+          require("fzf-lua").diagnostics_workspace()
         end,
-        desc = "Lists Diagnostics for all open buffers or a specific buffer in the workspace",
+        desc = "Lists Diagnostics for workspace",
       },
       {
         ";s",
         function()
-          local builtin = require("telescope.builtin")
-          builtin.lsp_document_symbols()
+          require("fzf-lua").lsp_document_symbols()
         end,
         desc = "Lists Function names, variables, from Treesitter",
       },
       {
         "sf",
         function()
-          local telescope = require("telescope")
-
-          local function telescope_buffer_dir()
-            return vim.fn.expand("%:p:h")
-          end
-
-          telescope.extensions.file_browser.file_browser({
-            path = "%:p:h",
-            cwd = telescope_buffer_dir(),
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            previewer = false,
-            initial_mode = "normal",
-            layout_config = { height = 40 },
-          })
+          require("fzf-lua").files({ cwd = vim.fn.expand("%:p:h") })
         end,
         desc = "Open File Browser with the path of the current buffer",
       },
     },
-    config = function(_, opts)
-      local telescope = require("telescope")
-      local actions = require("telescope.actions")
-      local fb_actions = require("telescope").extensions.file_browser.actions
-
-      -- Ensure opts.defaults is a table
-      opts.defaults = opts.defaults or {}
-
-      opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
-        wrap_results = true,
-        layout_strategy = "horizontal",
-        layout_config = { prompt_position = "top" },
-        sorting_strategy = "ascending",
-        winblend = 0,
-        mappings = {
-          n = {},
-        },
-      })
-      opts.pickers = {
-        diagnostics = {
-          theme = "ivy",
-          initial_mode = "normal",
-          layout_config = {
-            preview_cutoff = 9999,
-          },
-        },
-      }
-      opts.extensions = {
-        file_browser = {
-          theme = "dropdown",
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            -- your custom insert mode mappings
-            ["i"] = {
-              ["<A-c>"] = fb_actions.create,
-              ["<S-CR>"] = fb_actions.create_from_prompt,
-              ["<A-r>"] = fb_actions.rename,
-              ["<A-m>"] = fb_actions.move,
-              ["<A-y>"] = fb_actions.copy,
-              ["<A-d>"] = fb_actions.remove,
-              ["<C-o>"] = fb_actions.open,
-              ["<C-h>"] = fb_actions.goto_parent_dir,
-              ["<C-e>"] = fb_actions.goto_home_dir,
-              ["<C-w>"] = fb_actions.goto_cwd,
-              ["<C-t>"] = fb_actions.change_cwd,
-              ["<C-f>"] = fb_actions.toggle_browser,
-              ["<C-g>"] = fb_actions.toggle_hidden,
-              ["<C-s>"] = fb_actions.toggle_all,
-              ["<bs>"] = fb_actions.backspace,
-            },
-            --  Normal mode mappings
-            ["n"] = {
-              ["c"] = fb_actions.create,
-              ["r"] = fb_actions.rename,
-              ["m"] = fb_actions.move,
-              ["y"] = fb_actions.copy,
-              ["d"] = fb_actions.remove,
-              ["o"] = fb_actions.open,
-              ["h"] = fb_actions.goto_parent_dir,
-              ["e"] = fb_actions.goto_home_dir,
-              ["w"] = fb_actions.goto_cwd,
-              ["t"] = fb_actions.change_cwd,
-              ["f"] = fb_actions.toggle_browser,
-              ["g"] = fb_actions.toggle_hidden,
-              ["s"] = fb_actions.toggle_all,
-              ["/"] = function()
-                vim.cmd("startinsert")
-              end,
-              ["<C-u>"] = function(prompt_bufnr)
-                for i = 1, 10 do
-                  actions.move_selection_previous(prompt_bufnr)
-                end
-              end,
-              ["<C-d>"] = function(prompt_bufnr)
-                for i = 1, 10 do
-                  actions.move_selection_next(prompt_bufnr)
-                end
-              end,
-              ["<PageUp>"] = actions.preview_scrolling_up,
-              ["<PageDown>"] = actions.preview_scrolling_down,
-            },
-          },
-        },
-      }
-      telescope.setup(opts)
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("file_browser")
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
     end,
-  },
-  -- color picker
+  }, -- color picker
   {
     "ziontee113/color-picker.nvim",
     config = function()
@@ -296,21 +138,20 @@ return {
   -- { "vim-pandoc/vim-pandoc-syntax" },
   {
     "johmsalas/text-case.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
+    dependencies = { "ibhagwan/fzf-lua" },
     config = function()
       require("textcase").setup({})
-      require("telescope").load_extension("textcase")
     end,
     keys = {
       "ga", -- Default invocation prefix
-      { "ga.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x" }, desc = "Telescope" },
+      { "ga.", "<cmd>TextCaseOpenFzfLua<CR>", mode = { "n", "x" }, desc = "FZF" },
     },
     cmd = {
       -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
       "Subs",
-      "TextCaseOpenTelescope",
-      "TextCaseOpenTelescopeQuickChange",
-      "TextCaseOpenTelescopeLSPChange",
+      "TextCaseOpenFzfLua",
+      "TextCaseOpenFzfLuaQuickChange",
+      "TextCaseOpenFzfLuaLSPChange",
       "TextCaseStartReplacingCommand",
     },
     -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
