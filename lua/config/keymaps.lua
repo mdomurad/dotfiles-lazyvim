c53f793fe3010e15af5e51828f73f27821152206
md <marcindomurad@gmail.com>
2025-10-25 17:18:43 +0200

    refactor(codeCompanion): move keymaps from keymaps.lua to plugin config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -214,15 +214,6 @@ which_key.add({
 
 -- [AI]
 
--- [CodeCompanion]
-vim.keymap.set({ "n", "v" }, ";A", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, ";a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, ";h", "<cmd>CodeCompanionHistory<cr>", { noremap = true, silent = true })
-vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
-
--- Expand 'cc' into 'CodeCompanion' in the command line
-vim.cmd([[cab cc CodeCompanion]])
-
 -- [Copilot chat]
 -- if user ~= "ianus" then
 which_key.add({
