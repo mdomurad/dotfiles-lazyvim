b3550de995f255fb1ad918bf67f61eabbbbf1d06
md <marcindomurad@gmail.com>
2025-10-28 19:35:41 +0100

    Revert "feat(ConfiguratorViewModel.cs): add IsFamilyLoaded property and update logic"
    This reverts commit 9415354245a70f0514891b8849c3f71c688feb11.

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -20,6 +20,13 @@ return {
         "svelte-language-server",
         "python-lsp-server",
       })
+
+      -- Ensure opts.registries is a table
+      opts.registries = opts.registries or {}
+      vim.list_extend(opts.registries, {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      })
     end,
   },
 
