b92306ee3536b694df0696c1145b174b749e5a09
md <marcindomurad@gmail.com>
2025-10-28 19:33:46 +0100

    refactor(init.lua): remove net_easy_dotnet plugin from language plugins

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -1,6 +1,5 @@
 return {
   require("plugins.lang.net"),
-  require("plugins.lang.net_easy_dotnet"),
   require("plugins.lang.net_neotest"),
   require("plugins.lang.treesitter"),
   require("plugins.lang.html"),
