d5ece626445b55baab7d9e6f5ccdbdc92d1b1be3
md <marcindomurad@gmail.com>
2025-12-06 13:58:23 +0100

    chore(init.lua): comment out todo plugin in util module

--- lua/plugins/util/init.lua
+++ lua/plugins/util/init.lua
@@ -1,7 +1,7 @@
 return {
   -- require("plugins.util.neorg"),
   require("plugins.util.obsidian"),
-  require("plugins.util.todo"),
+  -- require("plugins.util.todo"),
   require("plugins.util.vim-coach"),
   require("plugins.util.flote"),
 }
