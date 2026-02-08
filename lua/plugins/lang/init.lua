367382acd48fec70c67aece1b03d2244789b80fe
md <marcindomurad@gmail.com>
2026-02-08 14:38:48 +0100

    chore(init.lua): add reference to improved Neovim config in comments

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -1,3 +1,6 @@
+-- setup based on :
+-- https://git.ramboe.io/YouTube/neovim-c-the-actually-improved-configuration-2025-roslynnvim-rzslnvim.git
+
 return {
   require("plugins.lang.net"),
   require("plugins.lang.net_roslyn"),
