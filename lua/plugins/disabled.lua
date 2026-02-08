62a71aca93cf6ee96bb0c3cc8de62cb20c1e728a
md <marcindomurad@gmail.com>
2026-02-08 14:12:11 +0100

    chore(lsp,disabled,dotnet): remove dotnet/omnisharp support and config

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,4 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "mfussenegger/nvim-dap-python", enabled = false },
-  { "Hoffs/omnisharp-extended-lsp.nvim", enabled = false },
 }
--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -70,55 +70,6 @@ return {
             },
           },
         },
-        omnisharp = {},
-        -- omnisharp = {
-        --   -- https://github.com/OmniSharp/omnisharp-roslyn/wiki/Configuration-Options
-        --   settings = {
-        --     FormattingOptions = {
-        --       -- Enables support for reading code style, naming convention and analyzer
-        --       -- settings from .editorconfig.
-        --       EnableEditorConfigSupport = true,
-        --       -- Specifies whether 'using' directives should be grouped and sorted during
-        --       -- document formatting.
-        --       OrganizeImports = true,
-        --     },
-        --     MsBuild = {
-        --       -- If true, MSBuild project system will only load projects for files that
-        --       -- were opened in the editor. This setting is useful for big C# codebases
-        --       -- and allows for faster initialization of code navigation features only
-        --       -- for projects that are relevant to code that is being edited. With this
-        --       -- setting enabled OmniSharp may load fewer projects and may thus display
-        --       -- incomplete reference lists for symbols.
-        --       LoadProjectsOnDemand = false,
-        --     },
-        --     RoslynExtensionsOptions = {
-        --       -- Enables support for roslyn analyzers, code fixes and rulesets.
-        --       EnableAnalyzersSupport = true,
-        --       -- Enables support for showing unimported types and unimported extension
-        --       -- methods in completion lists. When committed, the appropriate using
-        --       -- directive will be added at the top of the current file. This option can
-        --       -- have a negative impact on initial completion responsiveness,
-        --       -- particularly for the first few completion sessions after opening a
-        --       -- solution.
-        --       EnableImportCompletion = true,
-        --       -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-        --       -- true
-        --       AnalyzeOpenDocumentsOnly = false,
-        --       EnableDecompilationSupport = true,
-        --     },
-        --     Sdk = {
-        --       -- Specifies whether to include preview versions of the .NET SDK when
-        --       -- determining which version to use for project loading.
-        --       IncludePrereleases = true,
-        --       path = os.getenv("USERPROFILE") .. "\\scoop\\apps\\dotnet-sdk\\current",
-        --     },
-        --     RenameOptions = {
-        --       RenameInComments = false,
-        --       RenameOverloads = true,
-        --       RenameInStrings = false,
-        --     },
-        --   },
-        -- },
         tailwindcss = {
           root_dir = function(...)
             return require("lspconfig.util").root_pattern(".git")(...)
