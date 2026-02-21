da32599028500720dd101843dd8b7cd5f2a496bb
md <marcindomurad@gmail.com>
2026-02-21 17:17:12 +0100

    feat(todo.lua): add bullets.vim plugin with custom markdown keymaps

--- lua/plugins/util/todo.lua
+++ lua/plugins/util/todo.lua
@@ -1,9 +1,64 @@
 return {
-  {
-    "bngarren/checkmate.nvim",
-    ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
-    opts = {
-      -- files = { "*.md" }, -- any .md file (instead of defaults)
+  "dkarter/bullets.vim",
+  -- Lazy-load the plugin only on relevant filetypes
+  ft = { "markdown", "text", "gitcommit" },
+
+  -- Use LazyVim's native `keys` table to define your mappings
+  keys = {
+    {
+      ";x",
+      "<Plug>(bullets-toggle-checkbox)",
+      ft = "markdown",
+      desc = "Toggle Checkbox",
+      mode = { "n", "v" }, -- Works in Normal and Visual mode
+    },
+    {
+      "<leader>mN",
+      "<Plug>(bullets-renumber)",
+      ft = "markdown",
+      desc = "Renumber Bullets",
+      mode = { "n", "v" }, -- Works in Normal and Visual mode
+    },
+    {
+      "<leader>mx",
+      "<Plug>(bullets-toggle-checkbox)",
+      ft = "markdown",
+      desc = "Toggle Checkbox (Leader)",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>mo",
+      "<Plug>(bullets-newline)",
+      ft = "markdown",
+      desc = "New Bullet Below",
+      mode = { "n" },
+    },
+    {
+      "<leader>mO",
+      "<Plug>(bullets-newline)",
+      ft = "markdown",
+      desc = "New Bullet Below (Insert Mode)",
+      mode = { "i" },
+    },
+    {
+      "<leader>m>",
+      "<Plug>(bullets-demote)",
+      ft = "markdown",
+      desc = "Demote Bullet",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>m<",
+      "<Plug>(bullets-promote)",
+      ft = "markdown",
+      desc = "Promote Bullet",
+      mode = { "n", "v" },
     },
   },
+
+  init = function()
+    -- Remove trailing whitespace when toggling an empty checkbox
+    vim.g.bullets_checkbox_markers = " x"
+    vim.g.bullets_set_mappings = 0 -- Disable default mappings to avoid conflicts
+  end,
 }
