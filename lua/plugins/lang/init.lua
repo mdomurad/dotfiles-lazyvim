8b1106022b830c0920a74e80607609b9e96518b1
Marcin Domurad <marcin.domurad@hagergroup.com>
2026-05-04 13:15:16 +0200

    refactor(lang): move .NET plugin files into net/ subdirectory for clarity

--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -2,11 +2,11 @@
 -- https://git.ramboe.io/YouTube/neovim-c-the-actually-improved-configuration-2025-roslynnvim-rzslnvim.git
 
 return {
-  require("plugins.lang.net"),
-  require("plugins.lang.net_roslyn"),
-  require("plugins.lang.net_dap"),
-  require("plugins.lang.net_neotest"),
-  require("plugins.lang.net_easy_dotnet"),
+  require("plugins.lang.net.net"),
+  require("plugins.lang.net.roslyn"),
+  require("plugins.lang.net.dap"),
+  require("plugins.lang.net.neotest"),
+  require("plugins.lang.net.easy_dotnet"),
   require("plugins.lang.treesitter"),
   require("plugins.lang.html"),
 }
