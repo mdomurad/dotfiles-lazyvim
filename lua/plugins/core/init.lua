24f81c14d9fb1c12af3e04b1ec4bca8548e6a2bc
Marcin Domurad <marcin.domurad@hagergroup.com>
2026-05-04 14:54:12 +0200

    refactor(lang/markdown/img): move img plugin from core to markdown lang

--- lua/plugins/core/init.lua
+++ lua/plugins/core/init.lua
@@ -3,5 +3,4 @@ return {
   require("plugins.core.neogit"),
   require("plugins.core.database"),
   require("plugins.core.noice"),
-  require("plugins.core.img"),
 }
--- lua/plugins/lang/init.lua
+++ lua/plugins/lang/init.lua
@@ -8,6 +8,7 @@ return {
   require("plugins.lang.net.neotest"),
   require("plugins.lang.net.easy_dotnet"),
   require("plugins.lang.markdown.markdown-plus"),
+  require("plugins.lang.markdown.img"),
   require("plugins.lang.treesitter"),
   require("plugins.lang.html"),
 }
